302bb968f6c7e54f3e793cbecfd3140951ed5869
Carlos Macario <cmacariovx@gmail.com>
2023-05-03 18:26:41 -0400

    measures

--- server/app.js
+++ server/app.js
@@ -1,14 +1,35 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
 const authRouter = require("./routes/authRoutes")
 const userRouter = require("./routes/userRoutes")
 
 const app = express()
 
+app.use(helmet());
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+        },
+    })
+);
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin')
+
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
