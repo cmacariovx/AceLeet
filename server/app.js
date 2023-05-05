21a38d5636ff3bb99868055a8ad7fa733987708e
Carlos Macario <cmacariovx@gmail.com>
2023-05-05 11:51:10 -0400

    nginx for reverse proxy and ssl

--- server/app.js
+++ server/app.js
@@ -30,7 +30,13 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin')
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    const allowedOrigins = ['https://aceleet.io', 'https://www.aceleet.io'];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.indexOf(origin) > -1) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
 
@@ -41,8 +47,8 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use("/auth", authRouter)
-app.use("/user", userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 app.use((req, res, next) => {
     const error = new Error("Could not find this route.")
