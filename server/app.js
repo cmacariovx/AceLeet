eda4a2b4cff8100242cc23c585d1a9cac2ee5142
Carlos Macario <cmacariovx@gmail.com>
2023-05-08 10:23:25 -0400

    prod switch

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ const bodyParser = require("body-parser")
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
 
 const authRouter = require("./routes/authRoutes")
 const userRouter = require("./routes/userRoutes")
 
 require("dotenv").config()
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express()
 
@@ -23,8 +26,8 @@ app.use(
 );
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000,
+    max: 100,
 });
 
 app.use(limiter);
@@ -32,24 +35,28 @@ app.use(limiter);
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin')
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
 
-    const allowedOrigins = ['https://aceleet.io', 'https://www.aceleet.io'];
-    const origin = req.headers.origin;
+    if (isProduction) {
+        const allowedOrigins = ['https://aceleet.io', 'https://www.aceleet.io'];
+        const origin = req.headers.origin;
 
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
+        if (allowedOrigins.indexOf(origin) > -1) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
     }
 
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
     if (req.method === 'OPTIONS') {
-        return res.sendStatus(200)
+        return res.sendStatus(200);
     }
 
-    next()
-})
+    next();
+});
 
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
@@ -65,19 +72,23 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500)
     res.json({
         "message": error.message || "Unknown error occured.",
-        "stack": process.env.NODE_ENV === 'production' ? null : error.stack,
+        "stack": isProduction ? null : error.stack,
     });
 });
 
 
-const privateKey = fs.readFileSync('/app/certs/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/app/certs/fullchain.pem', 'utf8');
+if (isProduction) {
+    const privateKey = fs.readFileSync('/app/certs/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/app/certs/fullchain.pem', 'utf8');
 
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-};
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+    };
 
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(process.env.PORT || 5000);
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(process.env.PORT || 5000);
+} else {
+    const httpServer = http.createServer(app);
+    httpServer.listen(process.env.PORT || 5000);
+}
